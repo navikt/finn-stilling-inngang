807f0803aaa5fe8ed557f4c46af1724f7f3b48c9
Joar Aurdal <joar.aurdal@nav.no>
2023-06-26 14:33:20 +0200

    Bruk utviklingsapp

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,15 @@
-console.log('Hei, verden!');
+import { createRoot } from 'react-dom/client';
+import '@navikt/ds-css';
+import { BodyShort } from '@navikt/ds-react';
+import Navspa from '@navikt/navspa';
+
+const App = () => <BodyShort>Heihei</BodyShort>;
+
+if (import.meta.env.VITE_EXPORT) {
+    Navspa.eksporter('finn-stilling-inngang', App);
+} else {
+    const utviklingsapp = document.getElementById('finn-stilling-inngang');
+    const root = createRoot(utviklingsapp!);
+
+    root.render(<App />);
+}
