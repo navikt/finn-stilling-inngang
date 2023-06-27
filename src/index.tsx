4db3c1e07ae064cb4cbab3f4c30d7501e5de96e5
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-06-27 08:29:58 +0200

    Rename variabel til appName

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import Navspa from '@navikt/navspa';
 import App from './app/App';
 import './index.css';
 
-const elementId = 'finn-stilling-inngang';
+const appName = 'finn-stilling-inngang';
 
 if (import.meta.env.VITE_EXPORT) {
-    Navspa.eksporter(elementId, App);
+    Navspa.eksporter(appName, App);
 } else {
-    const utviklingsapp = document.getElementById(elementId);
+    const utviklingsapp = document.getElementById(appName);
     const root = createRoot(utviklingsapp!);
 
     root.render(<App fnr="123" />);
